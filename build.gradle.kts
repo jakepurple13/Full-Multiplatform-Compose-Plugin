66cb6daf05e2e79d8370101bac451484cfa10583
Jacob Rein <jakerein@gmail.com>
2023-11-13 07:30:43 -0500

    Update project and dependency versions, clean up properties
    Updated project version in build.gradle.kts along with Kotlin, AGP, Compose, and Ktor versions in NetworkVersions.kt and versions.json. Removed kotlin.native.enableDependencyPropagation and kotlin.mpp.enableGranularSourceSetsMetadata properties from project_gradle.properties.ft. These updates align the project with the latest version of the libraries and standards used, ensuring performance improvement and accessing new features or bug fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.programmersbox"
-version = "1.0.24"
+version = "1.0.25"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/programmersbox/fullmultiplatformcompose/utils/NetworkVersions.kt
+++ src/main/kotlin/com/programmersbox/fullmultiplatformcompose/utils/NetworkVersions.kt
@@ -40,11 +40,11 @@ class NetworkVersions {
 
 @Serializable
 data class ProjectVersions(
-    val kotlinVersion: String = "1.9.0",
+    val kotlinVersion: String = "1.9.10",
     val agpVersion: String = "7.3.0",
-    val composeVersion: String = "1.5.0",
+    val composeVersion: String = "1.5.10",
     val androidxAppCompat: String = "1.6.1",
     val androidxCore: String = "1.10.1",
-    val ktor: String = "2.3.5",
+    val ktor: String = "2.3.6",
     val koin: String = "3.4.0",
 )
